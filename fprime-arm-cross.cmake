505d9c72cf2b4cf77b718df4d380c98b3a282d8a
Paolo Oliveira <paolocmo@gmail.com>
2026-05-05 10:57:17 -0300

    fix flags

--- fprime-arm-cross.cmake
+++ fprime-arm-cross.cmake
@@ -22,8 +22,7 @@ set(CPU_FLAGS "-mcpu=cortex-m0plus -mthumb -mfloat-abi=soft")
 
 set(CMAKE_C_FLAGS_INIT    "${CPU_FLAGS} -ffunction-sections -fdata-sections")
 set(CMAKE_CXX_FLAGS_INIT  "${CPU_FLAGS} -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti")
-set(CMAKE_EXE_LINKER_FLAGS_INIT
-    "${CPU_FLAGS} -Wl,--gc-sections -Wl,-Map=pipicube.map")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${CPU_FLAGS}")
 
 # Sysroot search mode
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
